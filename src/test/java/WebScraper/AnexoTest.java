0a071dcf91c58a8d5ba9d111710d59e5a2290a4d
Gustavo Gaia <gu.gaia16@outlook.com>
2025-04-01 20:31:26 -0300

    1 commit - Criação classe anexo(correção testes iniciais)

--- src/test/java/WebScraper/AnexoTest.java
+++ src/test/java/WebScraper/AnexoTest.java
@@ -2,6 +2,7 @@ package test.java.WebScraper;
 
 
 import main.java.WebScraper.Anexo;
+import org.junit.Before;
 import org.junit.Test;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 
@@ -12,9 +13,10 @@ public class AnexoTest {
     private Anexo anexo1;
     private Anexo anexo2;
 
-    protected void setUp(){
-        anexo1 = new Anexo();
-        anexo2 = new Anexo();
+    @Before
+    public void setUp(){
+        anexo1 = new Anexo("Anexo_I_Rol_2021RN_465.2021_RN627L.2024","https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf");
+        anexo2 = new Anexo("Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025","https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf");
     }
 
     @Test
