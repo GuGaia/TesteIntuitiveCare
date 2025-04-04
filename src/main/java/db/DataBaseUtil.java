74bddabf71bf71f82fa0f82b90fe32677f80627c
Gustavo Gaia <gu.gaia16@outlook.com>
2025-04-04 14:52:44 -0300

    Esse commit cria a classe DataBaseUtil para executar a conexão com o database

--- src/main/java/db/DataBaseUtil.java
+++ src/main/java/db/DataBaseUtil.java
@@ -5,4 +5,17 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DataBaseUtil {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/database"; // Ajuste
+    private static final String USUARIO = "user"; // Ajuste
+    private static final String SENHA = "password"; // Ajuste
+
+    public static Connection conectar() throws Exception {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+    public static void runScript(String sql) throws Exception {
+        try (Connection conn = conectar(); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        }
+    }
 }
